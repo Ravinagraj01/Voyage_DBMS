8c6ac682215b072ca19897973907b99902170653
RohanMaroli <rohanmaroli1@gmail.com>
2024-05-24 14:51:47 +0530

    git commit -m 'message'

--- db_connection.php
+++ db_connection.php
@@ -4,9 +4,9 @@ $servername = "localhost"; // or your server hostname
 $username = "root"; // your MySQL username
 $password = ""; // your MySQL password
 $database = "tourist_recommendation"; // your database name
-
+$port="3306";
 // Create connection
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password, $database,$port);
 
 // Check connection
 //if ($conn) {
